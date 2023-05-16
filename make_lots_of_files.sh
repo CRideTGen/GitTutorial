082bd37680a37de9bf6e4371052c391faa13d7b5
Chase Ridenour <cridenour@tgen.org>
2023-05-16 16:12:33 -0700

    Run: make_lots_of_files.sh at 2023.05.16-16.11.04 with 1000 files

--- make_lots_of_files.sh
+++ make_lots_of_files.sh
@@ -9,10 +9,11 @@ mkdir -p lots_of_files
 script_name=$(basename "$0")
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 number_of_files=1000
-for i in seq "${number_of_files}"; do
+for i in $(seq 1 "${number_of_files}"); do
     echo "This is file ${i}" > lots_of_files/my_output_${i}.txt
 done
 
+git add "$0"
 git add lots_of_files
 git commit -m "Run: ${script_name} at ${current_time} with ${number_of_files} files"
 
