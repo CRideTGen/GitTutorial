ddab0261161eac0f0f54e8fa54bf7eaf125d0c95
Chase Ridenour <cridenour@tgen.org>
2023-05-16 16:21:40 -0700

    Run: make_lots_of_files.sh at 2023.05.16-16.20.07 with 1000 files

--- make_lots_of_files.sh
+++ make_lots_of_files.sh
@@ -9,17 +9,20 @@ mkdir -p lots_of_files
 script_name=$(basename "$0")
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 number_of_files=1000
+echo "starting making files"
 for i in $(seq 1 "${number_of_files}"); do
   echo "This is file ${i}" >lots_of_files/my_output_${i}.txt
 done
 
+echo "finished making files"
 git add "$0"
-git add lots_of_files
+git add lots_of_files/*
+echo "added files to git"
 
 if git diff-index --quiet HEAD --; then
   echo "No changes to commit"
   exit 0
 else
-
+  echo "committing files"
   git commit -m "Run: ${script_name} at ${current_time} with ${number_of_files} files"
 fi
