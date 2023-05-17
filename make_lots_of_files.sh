36166df10f08954b9233fbfa12ff1bdd4a0e69dc
Chase Ridenour <cridenour@tgen.org>
2023-05-16 17:00:59 -0700

    added removal and recreation of slurm_logs and added jobid to commit messgage

--- make_lots_of_files.sh
+++ make_lots_of_files.sh
@@ -8,17 +8,23 @@ if [[ -d lots_of_files ]]; then
 fi
 mkdir -p lots_of_files
 
+if [[ -d slurm_logs ]]; then
+  rm -rf slurm_logs
+fi
+mkdir -p slurm_logs
+
 script_name=$(basename "$0")
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 number_of_files=100
 
 echo "starting making files"
-sbatch --wait --job-name="Making_Files" --output=slurm_logs/slurm_%A_%a.out --array=1-"${number_of_files}"%100 ./slurm_array.sh
+jobid=$(sbatch --wait --job-name="Making_Files" --output=slurm_logs/slurm_%A_%a.out --array=1-"${number_of_files}"%100 ./slurm_array.sh)
 
 
 echo "finished making files"
 git add "$0"
 git add lots_of_files/*
+git add slurm_logs/*
 echo "added files to git"
 
 if git diff-index --quiet HEAD --; then
@@ -26,5 +32,5 @@ if git diff-index --quiet HEAD --; then
   exit 0
 else
   echo "committing files"
-  git commit -m "Run: ${script_name} at ${current_time} with ${number_of_files} files"
+  git commit -m "Run: ${script_name} jobid=${jobid} at ${current_time} with ${number_of_files} files"
 fi
